d2cf3edf3c23a458fa60f51bc240ee2608ed3873
Yuri Paz <yuri.paz@outlook.com>
2021-11-06 17:51:35 -0300

    Criação do Arquivo Cypress + Base para os testes

---
+++ cypress/integration/exercicioDeTeste.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress"/>
+describe('Exercicio de entrada e saída de dados', () => {
+  beforeEach('', () => {
+    cy.visit('https://devfinances.tk/#/');
+  });
+
+  it('Teste de Entrada de Dados', () => {});
+
+  it('Teste de exclusão de dados', () => {});
+});
